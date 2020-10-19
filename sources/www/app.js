aaca7857e0c311a18e772128ca366df2e668b119
Emrudia <elisabarre@orange.fr>
2020-10-19 14:16:24 +0200

    modif node

--- sources/www/app.js
+++ sources/www/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const hostname = 'localhost'
+const port = 8095
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://${hostname}:${port}`)
 })
